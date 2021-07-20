50979d1c2f9c4e930539b0eda3e8dd7510d1305d
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2021-07-20 16:39:32 +0200

    Basics types

--- multimath/app/app.ts
+++ multimath/app/app.ts
@@ -1,6 +1,11 @@
 function startGame() {
     const messagesElement = document.getElementById('messages');
+    const playerName: string = 'Audrey';
+    logPLayer(playerName);
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+function logPLayer(name) {
+    console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
