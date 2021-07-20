786a978c95a64a392a297e3551ff590a7819614c
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2021-07-20 17:04:34 +0200

    Tipes to functions

--- multimath/app/app.ts
+++ multimath/app/app.ts
@@ -1,11 +1,27 @@
-function startGame() {
-    const messagesElement = document.getElementById('messages');
-    let playerName: string = 'Audrey';
+function startGame(): void {
+    
+    const playerName: string | undefined = getInputValue('playername');
     logPLayer(playerName);
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+    
+    postScore(100, playerName);
 }
-function logPLayer(name) {
+
+function logPLayer(name: string = 'MultiMath Player'): void {
     console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementId: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
+    if(inputElement.value === '') {
+        return undefined;
+    } else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName?: string): void {
+    const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
